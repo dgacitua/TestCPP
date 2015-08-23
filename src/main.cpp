f7141b82c29a2a544cb41fc7e994a787e6826485
Daniel Gacitua <danielgacituav@gmail.com>
2015-08-23 18:10:12 -0300

    Agregado Hola Mundo

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 23-08-2015
+ *      Author: Daniel Gacitúa
+ */
+
+#include <iostream>
+
+int main() {
+	// Imprimir "Hola Mundo!"
+	std::cout << "Hola Mundo!" << std::endl;
+
+	// Retorno del main
+	return 0;
+}
+
